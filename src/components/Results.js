3934372c2e0ec2f09f93c00c139fff1af8ba78b4
Roberto Jr <junionascimento2001@gmail.com>
2020-04-25 21:01:43 -0300

    done

---
+++ src/components/Results.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Result from './Result';
+
+export default function Results ({ results, openPopup }) {
+    return (
+      <section className="results">
+        {results.map(result => (
+          <Result 
+            key={result.imdbID} 
+            result={result} 
+            openPopup={openPopup} 
+          />
+        ))}
+      </section>
+    );
+}
